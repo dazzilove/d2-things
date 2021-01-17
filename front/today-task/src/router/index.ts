2e379d7c0b498ce61b50229d4deb925833f0fe65
1100303 <dazzilove@sk.com>
2021-01-17 23:34:38 +0900

    태스크관리 라우팅 추가

--- front/today-task/src/router/index.ts
+++ front/today-task/src/router/index.ts
@@ -7,17 +7,19 @@ Vue.use(VueRouter);
 const routes: RouteConfig[] = [
   {
     path: '/',
+    name: 'Main',
+    component: TodayTask,
+  },
+  {
+    path: '/todayTask',
     name: 'TodayTask',
     component: TodayTask,
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
+  {
+    path: '/taskMng',
+    name: 'TaskMng',
+    component: () => import('@/views/TaskMng.vue'),
+  },
 ];
 
 const router = new VueRouter({
